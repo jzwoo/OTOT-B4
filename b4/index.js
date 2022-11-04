08ba53c9e38c082ba4def37719fd6b4ceba1843c
jzwoo <jzwoo98@gmail.com>
2022-11-04 21:21:13 +0800

    Change format of response

--- b4/index.js
+++ b4/index.js
@@ -1,24 +1,24 @@
-const axios = require("axios")
+const axios = require('axios')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const URL = `https://api.apilayer.com/exchangerates_data/latest?symbols=&base=SGD`
 
-    const response = await axios.get(URL, { headers: { apiKey: "U0pJ3hyaJ6c78eroU7OsAc2fx6Yc88mT" }})
-    const { success, rates } = response.data;
+    const response = await axios.get(URL, {headers: {apiKey: 'U0pJ3hyaJ6c78eroU7OsAc2fx6Yc88mT'}})
+    const {success, rates} = response.data;
 
     if (!success) {
         context.res = {
             status: 404,
-            body: { message: "Error" }
+            body: {message: 'Error'}
         };
         return;
     }
 
-    const asianCurrenciesTop10GDPPerCapita = ["SGD", "QAR", "AED", "CNY", "BND", "HKD", "TWD", "BHD", "SAR", "KRW"]
+    const asianCurrenciesTop10GDPPerCapita = ['SGD', 'QAR', 'AED', 'CNY', 'BND', 'HKD', 'TWD', 'BHD', 'SAR', 'KRW']
 
-    const filteredRates = Object.keys(rates).filter((key) =>  asianCurrenciesTop10GDPPerCapita.includes(key)).reduce((obj, key) => {
+    const filteredRates = Object.keys(rates).filter((key) => asianCurrenciesTop10GDPPerCapita.includes(key)).reduce((obj, key) => {
         return Object.assign(obj, {
             [key]: rates[key]
         });
@@ -26,6 +26,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { data: filteredRates }
+        body: filteredRates
     };
 }
