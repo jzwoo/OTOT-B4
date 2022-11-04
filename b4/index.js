9c1fdc1e6d6b1fefae11baff12c0ef0ab315c315
jzwoo <jzwoo98@gmail.com>
2022-11-04 18:28:37 +0800

    Implement filtering

--- b4/index.js
+++ b4/index.js
@@ -1,13 +1,31 @@
+const axios = require("axios")
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const URL = `https://api.apilayer.com/exchangerates_data/latest?symbols=&base=SGD`
+
+    const response = await axios.get(URL, { headers: { apiKey: "U0pJ3hyaJ6c78eroU7OsAc2fx6Yc88mT" }})
+    const { success, rates } = response.data;
+
+    if (!success) {
+        context.res = {
+            status: 404,
+            body: { message: "Error" }
+        };
+        return;
+    }
+
+    const asianCurrenciesTop10GDPPerCapita = ["SGD", "QAR", "AED", "CNY", "BND", "HKD", "TWD", "BHD", "SAR", "KRW"]
+
+    const filteredRates = Object.keys(rates).filter((key) =>  asianCurrenciesTop10GDPPerCapita.includes(key)).reduce((obj, key) => {
+        return Object.assign(obj, {
+            [key]: rates[key]
+        });
+    }, {})
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: { data: filteredRates }
     };
 }
